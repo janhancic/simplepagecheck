377db6ba67a172cc2287bc871b98af0d3872212c
Jan Hancic <jan@popcom.si>
2010-10-20 12:41:40 +0200

    UPD: SimplePageCheck
    * add 'urlExists' method
    * add code for running checks (RunChecks)

--- SimplePageCheck.php
+++ SimplePageCheck.php
@@ -2,7 +2,7 @@
 
 class SimplePageCheck
 {
-	protected $sites = Array ();
+	protected $checks = Array ();
 
 	public function AddCheck ( $Url, $StringToCheck, $IsRegex = false )
 	{
@@ -12,7 +12,7 @@ class SimplePageCheck
 		if ( Is_Bool ( $IsRegex ) === false )
 			throw new InvalidArgumentException ( '$IsRegex "' . $IsRegex . '" is not a boolean.' );
 
-		$this->sites[] = Array (
+		$this->checks[] = Array (
 			'url' => $Url,
 			'check' => $StringToCheck,
 			'is_regex' => $IsRegex
@@ -21,7 +21,63 @@ class SimplePageCheck
 
 	public function RunChecks ()
 	{
-		
+		if ( Count ( $this->checks ) === 0 )
+			throw new BadMethodCallException ( 'No checks defined. Add at least one check using "AddCheck" method' );
+
+		foreach ( $this->checks as $key => $check )
+		{
+			if ( $this->urlExists ( $check['url'] ) === false )
+			{
+				$this->sendError ( $check, 'URL not reachable' );
+
+				continue;
+			}
+
+			$response = File_Get_Contents ( $check['url'] );
+
+			if ( $response === false )
+			{
+				$this->sendError ( $check, 'URL not reachable' );
+
+				continue;
+			}
+
+			$checkFound = false;
+			if ( $check['is_regex'] === false )
+				$checkFound = ( StrPos ( $response, $check['check'] ) !== false );
+
+			if ( $checkFound === false )
+				$this->sendError ( $check, 'Check string not found' );
+		}
+	}
+
+	protected function sendError ( $Check, $ErrorMessage )
+	{
+		echo 'error' . "\n";
+	}
+
+	protected function urlExists ( $Url )
+	{
+		if ( Function_Exists ( 'curl_init' ) === false )
+			return true;
+		else
+		{
+			// from PHP's manual: http://www.php.net/manual/en/function.file-exists.php#85246
+			$handle = CUrl_Init ( $Url );
+			if ( $handle === false )
+				return false;
+
+			CUrl_SetOpt ( $handle, CURLOPT_HEADER, false );
+			CUrl_SetOpt ( $handle, CURLOPT_FAILONERROR, true );
+			CUrl_SetOpt ( $handle, CURLOPT_HTTPHEADER, Array ( "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15" ) );
+			CUrl_SetOpt ( $handle, CURLOPT_NOBODY, true );
+			CUrl_SetOpt ( $handle, CURLOPT_RETURNTRANSFER, false );
+
+			$connectable = CUrl_Exec ( $handle );
+			CUrl_Close ( $handle );
+
+			return $connectable;
+		}
 	}
 }
 
