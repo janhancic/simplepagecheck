3f4beb9084871ef8a6dce94354c72e75914636ad
Jan Hancic <jan@popcom.si>
2010-10-23 17:15:58 +0200

    UPD: update example.php to reflect changes in SimplePageCheck class

--- example.php
+++ example.php
@@ -2,12 +2,21 @@
 
 Include ( 'SimplePageCheck.php' );
 
-$spc = new SimplePageCheck ( 'your@email.com' );
+// initialize a new SimplePageCheck object
+$spc = new SimplePageCheck ();
 
+// tell SimplePageCheck to send errors to your@email.com and to send one email with all errors
+$spc->ReportToEmail ( 'your@email.com', false );
+
+// tell SimplePageCheck to output errors to the standard output
+$spc->ReportToOutput ( true );
+
+// add some checks
 $spc->AddCheck ( 'http://www.example.com', null, 'page by typing' );
 $spc->AddCheck ( 'http://www.example.org', null, 'Section 3test.' ); // this will fail
+$spc->AddCheck ( 'http://www.example.org', 'http://www.example.org', '/You (.*) testreached/is', true ); // this too will fail
 
-
+// run all the added checks
 $spc->RunChecks ();
 
 ?>
